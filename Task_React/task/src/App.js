732fe6d5ac76d4517070fc2f630f81ff3cf67456
DanielRabeloChaves <danielrabelochaves1999@gmail.com>
2021-05-21 22:09:01 -0300

    Projeto Integrador

---
+++ Task_React/task/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Login from './pages/Login/login'
+
+
+function App() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ Task_React/task/src/pages/Login/login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './login.css';
+
+function Login() {
+    return (
+        <div className="login">
+
+        </div>
+        
+    )
+
+}
+
+
+export default Login;
